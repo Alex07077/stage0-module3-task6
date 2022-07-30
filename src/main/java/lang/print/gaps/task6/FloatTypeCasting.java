ab99b5c24cef02ca05dc6f8d9576c1e74cd7fb27
Alex Myachin <myachin_aleksei@mail.ru>
2022-07-30 23:59:06 +0600

    Type Casting.

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        float i = (numberToBeRounded - ((int)numberToBeRounded));
+            if (i > 0.499999){
+                System.out.println((int)numberToBeRounded + 1);
+            }else{
+                System.out.println((int)numberToBeRounded);
+            }
     }
 }
